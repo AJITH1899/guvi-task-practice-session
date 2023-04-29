fdd2c6ed139df8e5cd79e5c20ff26cfc04f9bd43
AJITH1899 <ajithdancer1899@gmail.com>
2023-04-29 17:09:19 +0530

    guvi day3 task

---
+++ script.js
@@ -0,0 +1,29 @@
+let myresume = {
+    'basic information': {
+        "Name":"Ajith R",
+        "age":23,
+        "Education":"diploma(mech) 2nd year discontinued",
+        "Email":"ajithdancer1899@gmail.com"
+
+    },
+    "profile link":{
+        "github":"https://github.com/AJITH1899"
+    },
+    "working experice":{
+        "company":"hochste health care pvt limited",
+        "position":"custom made designer in cadcam",
+        "manufacturing":"custom  ortho footwears",
+        "experience":"1.5 years"
+    },
+
+    "languages":{
+          "language": "Tamil"
+    },
+    
+    "hoobies":{
+        "hoobies":"playing cricket and video games"
+    }
+   
+
+}
+console.log(myresume);
